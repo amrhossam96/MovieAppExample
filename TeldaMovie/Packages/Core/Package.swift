dcb426616478198a1ceb4b0bfca67c8bb2c05549
Amr Hossam <amrhossam96@hotmail.com>
2024-11-04 23:29:34 +0200

    init commit

---
+++ TeldaMovie/Packages/Core/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Core",
+    platforms: [.iOS(.v14)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Core",
+            targets: ["Core"]),
+    ],
+    dependencies: [
+        .package(path: "../../Networking"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Core",
+            dependencies: [
+                .product(name: "Networking", package: "Networking")
+            ]
+        ),
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["Core"]
+        ),
+    ]
+)
