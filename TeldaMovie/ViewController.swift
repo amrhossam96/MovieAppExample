6ebf15184ab89a412e08a7c56a8114f050a12ab3
Amr Hossam <amrhossam96@hotmail.com>
2024-11-04 23:17:45 +0200

    Initial Commit

---
+++ TeldaMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeldaMovie
+//
+//  Created by Amr on 04/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
