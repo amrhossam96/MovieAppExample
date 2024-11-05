dea646ab29f43fab51aab8cd9ff9cd870ec69147
Amr Hossam <amrhossam96@hotmail.com>
2024-11-05 21:45:44 +0200

    removing netfox

--- TeldaMovie/Root/AppDelegate.swift
+++ TeldaMovie/Root/AppDelegate.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
-import netfox
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        NFX.sharedInstance().start()
+
         return true
     }
 
